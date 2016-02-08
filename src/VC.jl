5cc9683dbb290fa917141b3dd4436a45e65d507f
Robert DJ <git@dahl-jacobsen.dk>
2016-02-08 12:57:17 +0100

    Functions for line intersection

--- src/VC.jl
+++ src/VC.jl
@@ -1,6 +1,19 @@
-typealias Points2D Array{GeometricalPredicates.Point2D,1}
+typealias Points2D Array{VoronoiDelaunay.Point2D,1}
 # Hash table of Voronoi cells indexed by their generators
 typealias VoronoiCorners Dict{Point2D, Points2D}
+
+# Edges of the bounding box
+const left = VoronoiDelaunay.min_coord
+const right = VoronoiDelaunay.max_coord
+const lower = VoronoiDelaunay.min_coord
+const upper = VoronoiDelaunay.max_coord
+
+# Corners of the bounding box
+const lowerleft  = Point2D( VoronoiDelaunay.min_coord, VoronoiDelaunay.min_coord )
+const lowerright = Point2D( VoronoiDelaunay.max_coord, VoronoiDelaunay.min_coord )
+const upperright = Point2D( VoronoiDelaunay.max_coord, VoronoiDelaunay.max_coord )
+const upperleft  = Point2D( VoronoiDelaunay.min_coord, VoronoiDelaunay.max_coord )
+
 
 function Base.show(io::IO, C::VoronoiCorners)
 	println("Generator    Corners")
@@ -29,7 +42,7 @@ end
 @doc """
 	contains(p::Point2D, P::Points2D) -> Bool
 
-Test if `p` is in `P`.
+Test if the point `p` is in the list of points `P`.
 """->
 function contains(p::Point2D, P::Points2D)
 	for element in P
@@ -75,6 +88,8 @@ function newcorner!(corners::VoronoiCorners, generator::Point2D, corner::Point2D
 end
 
 @doc """
+	corners(generators::Points2D) -> VoronoiCorners
+
 Collect the Voronoi cells from set of `generators`.
 """->
 function corners(generators::Points2D)
@@ -91,15 +106,126 @@ function corners(generators::Points2D)
 	for edge in voronoiedges(tess)
 		# TODO: Leave out the corners of the bounding box
 		generator = getgena(edge)
-		newcorner!(corners, generator, geta(edge))
-		newcorner!(corners, generator, getb(edge))
+		#= if !is_bounding_corner(generator) =#
+			newcorner!(corners, generator, geta(edge))
+			newcorner!(corners, generator, getb(edge))
+		#= end =#
 
 		generator = getgenb(edge)
-		newcorner!(corners, generator, geta(edge))
-		newcorner!(corners, generator, getb(edge))
+		#= if !is_bounding_corner(generator) =#
+			newcorner!(corners, generator, geta(edge))
+			newcorner!(corners, generator, getb(edge))
+		#= end =#
 	end
 
 	# TODO: Orientation of the polygons/cells
 	return corners
+end
+
+@doc """
+	is_bounding_corner(p::Point2D) -> Bool
+
+Test if `p` is a corner in the bounding box.
+"""->
+function is_bounding_corner(p::Point2D)
+	# TODO: test == or isapprox?
+	isapprox(p, lowerleft) || isapprox(p, lowerright) || isapprox(p, upperleft) || isapprox(p, upperright)
+end
+
+@doc """
+	isvertical(gena::Point2D, genb::Point2D)
+
+Test if line segment with endpoints `gena` and `genb` is vertical.
+"""->
+function isvertical(gena::Point2D, genb::Point2D)
+	isapprox( getx(gena), getx(genb) )
+end
+
+@doc """
+	ishorizontal(gena::Point2D, genb::Point2D)
+
+Test if line segment with endpoints `gena` and `genb` is horizontal.
+"""->
+function ishorizontal(gena::Point2D, genb::Point2D)
+	isapprox( gety(gena), gety(genb) )
+end
+
+@doc """
+	intersection(edge::Symbol, gena::Point2D, genb::Point2D) -> Point2D
+
+Compute the intersection point between a line of the boundary box and the line segment with endpoints `gena` and `genb`.
+
+`edge` can be `:left`, `:right`, `:lower` or `:upper`.
+"""->
+function intersection(edge::Symbol, gena::Point2D, genb::Point2D)
+	if edge == :lower
+		yvalue = VoronoiDelaunay.min_coord
+		xvalue = xintersection(yvalue, gena, genb)
+	elseif edge == :upper
+		yvalue = VoronoiDelaunay.max_coord
+		xvalue = xintersection(yvalue, gena, genb)
+	elseif edge == :left
+		xvalue = VoronoiDelaunay.min_coord
+		yvalue = yintersection(xvalue, gena, genb)
+	elseif edge == :right
+		xvalue = VoronoiDelaunay.max_coord
+		yvalue = yintersection(xvalue, gena, genb)
+	else
+		error("Edge must be :left, :right, :lower or :upper")
+	end
+
+	return Point2D(xvalue,yvalue)
+end
+
+@doc """
+	xintersection(yvalue::Float64, gena::Point2D, genb::Point2D) -> x
+
+Compute the `x`-value of the intersection between the horizontal line `y=yvalue` and the straight line through `gena` and `genb`.
+"""->
+function xintersection(yvalue::Float64, gena::Point2D, genb::Point2D)
+	if isvertical(gena,genb)
+		# @assert !isapprox(gena,genb) "Supply two different points"
+		xvalue = getx(gena)
+	else
+		slope, intersect = line(gena,genb)
+		xvalue = (yvalue - intersect)/slope
+	end
+
+	return xvalue
+end
+
+@doc """
+	yintersection(xvalue::Float64, gena::Point2D, genb::Point2D) -> y
+
+Compute the `y`-value of the intersection between the vertical line `x=xvalue` and the (non-vertical) straight line through `gena` and `genb`.
+"""->
+function yintersection(xvalue::Float64, gena::Point2D, genb::Point2D)
+	slope, intersect = line(gena,genb)
+	return xvalue*slope + intersect
+end
+
+@doc """
+	line(gena::Point2D, genb::Point2D) -> (slope,intersect)
+
+Compute the slope and intersection of the straight line that goes through `gena` and `genb`.
+"""->
+function line(gena::Point2D, genb::Point2D)
+	@assert !isapprox(gena,genb) "Supply two different points"
+	@assert !isvertical(gena,genb) "Line segment is vertical"
+
+	ax = getx(gena)
+	ay = gety(gena)
+	slope = (ay - gety(genb)) / (ax - getx(genb))
+	intersect = ay - slope*ax
+	return (slope,intersect)
+end
+
+@doc """
+	isinside(corner::Point2D)
+
+Test if `corner` is inside the bounding box.
+"""->
+function isinside(corner::Point2D)
+	left <= getx(corner) <= right && lower <= gety(corner) <= upper
 end
 
