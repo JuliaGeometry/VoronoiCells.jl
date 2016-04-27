ea18c0884b5716ad3ae3a9009019acf8f8cc476e
Robert DJ <git@dahl-jacobsen.dk>
2016-04-27 14:11:02 +0200

    Voronoi areas for vectors
    To provide the same interface as in the Deldir package

--- src/Area.jl
+++ src/Area.jl
@@ -1,4 +1,35 @@
 @doc """
+	voronoiarea(x::Vector, y::Vector; rw) -> Vector
+"""->
+function voronoiarea(x::Vector{Float64}, y::Vector{Float64}; rw::Vector{Float64}=[0.0;1.0;0.0;1.0])
+	@assert (N = length(x)) == length(y)
+
+	# Scale x and y to fit the window from GeometricalPredicates
+	# TODO: No need to save these
+	const minx = minimum(x)
+	const miny = minimum(y)
+	const maxx = maximum(x)
+	const maxy = maximum(y)
+
+	const RW_LEFT = rw[1]
+	const RW_LOWER = rw[3]
+	@assert minx >= RW_LEFT && maxx <= rw[2] && miny >= RW_LOWER && maxy <= rw[4]
+
+	const scalex = rw[2] - RW_LEFT
+	const scaley = rw[4] - RW_LOWER
+
+	pts = [IndexablePoint( left + (x[n]-RW_LEFT)/scalex, lower + (y[n]-RW_LOWER)/scaley, n) for n = 1:N]
+
+	# Areas for scaled points
+	C = corners(pts)
+	A = voronoiarea(C)
+
+	scale!(A, scalex*scaley)
+
+	return A
+end
+
+@doc """
 	voronoiarea(C::VoronoiCorners) -> Vector
 
 Compute the area of each of the Voronoi cells in `C`.
@@ -25,14 +56,14 @@ Compute the area of the polygon with vertices `p` using the shoelace formula.
 
 If the points in `p` are not sorted, they will be sorted **in-place**.
 """->
-function polyarea{T<:AbstractPoint2D}(p::Vector{T})
-	issorted(p) || sort!(p)
+function polyarea{T<:AbstractPoint2D}(pts::Vector{T})
+	issorted(pts) || sort!(pts)
 
-	Np = length(p)
-	A = getx(p[1])*( gety(p[2]) - gety(p[Np]) ) + getx(p[Np])*( gety(p[1]) - gety(p[Np-1]) )
+	Np = length(pts)
+	A = getx(pts[1])*( gety(pts[2]) - gety(pts[Np]) ) + getx(pts[Np])*( gety(pts[1]) - gety(pts[Np-1]) )
 
 	for n in 2:Np-1
-		A += getx(p[n])*( gety(p[n+1]) - gety(p[n-1]) )
+		A += getx(pts[n])*( gety(pts[n+1]) - gety(pts[n-1]) )
 	end
 
 	return 0.5*abs(A)
@@ -57,7 +88,13 @@ function Base.mean{T<:AbstractPoint2D}(pts::Vector{T})
 	Point2D(ax/Np, ay/Np)
 end
 
-function Base.issorted(pts::AbstractPoints2D)
+function Base.sort!{T<:AbstractPoint2D}(pts::Vector{T})
+	center = mean(pts)
+	centralize = p -> p - center
+	sort!( pts, by=centralize )
+end
+
+function Base.issorted{T<:AbstractPoint2D}(pts::Vector{T})
 	center = mean(pts)
 	centralize = p -> p - center
 	issorted( pts, by=centralize )
@@ -92,8 +129,7 @@ end
 
 # Sorting by polar angle around the origin.
 # http://gamedev.stackexchange.com/questions/13229/sorting-array-of-points-in-clockwise-order/
-# The sign of atan2 makes this comparison inadequate for issorted, but
-# fine for sort
+# The sign of atan2 makes this comparison inadequate for issorted, but fine for sort
 function isless_angle(p::AbstractPoint2D, q::AbstractPoint2D)
 	atan2(gety(p), getx(p)) > atan2(gety(q), getx(q))
 end
