c10b9554072cb295655e93f82ca87b38f5b5ad05
Robert DJ <git@dahl-jacobsen.dk>
2016-05-03 13:54:04 +0200

    Checking out latest VoronoiDelaunay in Travis
    Only using Deldir for testing if it is installed

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,5 @@
 using VoronoiCells
 import VoronoiCells: LEFT, RIGHT, LOWER, UPPER
-using Deldir
 using Base.Test
 
 # write your own tests here
@@ -11,17 +10,20 @@ y = 1.0 + rand(N)
 A = VoronoiCells.voronoiarea(x, y; rw=[LEFT; RIGHT; LOWER; UPPER])
 
 # A weak test until the corner issue is fixed
-@test sum(A) <= 1.0
 #= @test_approx_eq sum(A) 1.0 =#
+@test sum(A) <= 1.0
 
 
 # ------------------------------------------------------------
 # I trust Deldir: With the corner points added, VoronoiCells and
 # Deldir shoud return the same tesselation
 
-x2 = [x ; RIGHT ; LEFT ; LEFT ; RIGHT]
-y2 = [y ; UPPER ; UPPER ; LOWER ; LOWER]
+if !isa(Pkg.installed("Deldir"), Void)
+	x2 = [x ; RIGHT ; LEFT ; LEFT ; RIGHT]
+	y2 = [y ; UPPER ; UPPER ; LOWER ; LOWER]
 
-A2 = Deldir.voronoiarea(x2, y2; rw=[LEFT; RIGHT; LOWER; UPPER])
-@test_approx_eq A A2[1:N]
+	using Deldir
+	A2 = Deldir.voronoiarea(x2, y2; rw=[LEFT; RIGHT; LOWER; UPPER])
+	@test_approx_eq A A2[1:N]
+end
 
