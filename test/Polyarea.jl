ae5089e6b6345d5e53650e0a0decce43bd6b15db
Robert DJ <git@dahl-jacobsen.dk>
2016-05-03 22:00:00 +0200

    Test different functions in separate files

---
+++ test/Polyarea.jl
@@ -0,0 +1,16 @@
+using VoronoiCells
+using VoronoiDelaunay
+import VoronoiCells: LEFT, RIGHT, LOWER, UPPER
+using Base.Test
+
+# Area of square
+S = [Point2D(LEFT,LOWER) ; Point2D(RIGHT,LOWER) ; Point2D(RIGHT,UPPER) ; Point2D(LEFT,UPPER)]
+@test_approx_eq polyarea(S) 1.0
+
+# Area of triangle
+T1 = [Point2D(LEFT,LOWER) ; Point2D(RIGHT,LOWER) ; 0.5*(Point2D(RIGHT,UPPER) + Point2D(LEFT,UPPER))]
+@test_approx_eq polyarea(T1) 0.5
+
+T2 = [Point2D(LEFT,LOWER) ; Point2D(LEFT+0.5,LOWER) ;
+Point2D(LEFT,LOWER+0.5)]
+@test_approx_eq polyarea(T2) 0.5^3
---
+++ test/Voronoiarea.jl
@@ -0,0 +1,28 @@
+using VoronoiCells
+import VoronoiCells: LEFT, RIGHT, LOWER, UPPER
+using Base.Test
+
+N = 100
+x = 1.0 + rand(N)
+y = 1.0 + rand(N)
+
+A = VoronoiCells.voronoiarea(x, y; rw=[LEFT; RIGHT; LOWER; UPPER])
+
+# A weak test until the corner issue is fixed
+#= @test_approx_eq sum(A) 1.0 =#
+@test sum(A) <= 1.0
+
+
+# ------------------------------------------------------------
+# I trust Deldir: With the corner points added, VoronoiCells and
+# Deldir shoud return the same tesselation
+
+if !isa(Pkg.installed("Deldir"), Void)
+	x2 = [x ; RIGHT ; LEFT ; LEFT ; RIGHT]
+	y2 = [y ; UPPER ; UPPER ; LOWER ; LOWER]
+
+	using Deldir
+	A2 = Deldir.voronoiarea(x2, y2; rw=[LEFT; RIGHT; LOWER; UPPER])
+	@test_approx_eq A A2[1:N]
+end
+
