3353116a97d2547561c9700d23f57418aeb47a70
Robert DJ <git@dahl-jacobsen.dk>
2016-06-23 15:06:23 +0200

    Fixing test file

--- test/Voronoiarea.jl
+++ test/Voronoiarea.jl
@@ -6,7 +6,7 @@ N = 100
 x = 1.0 + rand(N)
 y = 1.0 + rand(N)
 
-A = VoronoiCells.voronoiarea(x, y; rw=[LEFT; RIGHT; LOWER; UPPER])
+A = VoronoiCells.voronoiarea(x, y, [LEFT; RIGHT; LOWER; UPPER])
 @test_approx_eq sum(A) 1.0
 #= @test sum(A) <= 1.0 =#
 
