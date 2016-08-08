4bfe0cffd3822b07275b0224cbf8175afcd28ddc
Robert DJ <git@dahl-jacobsen.dk>
2016-08-08 10:30:59 +0200

    Commenting test script

--- test/Voronoiarea.jl
+++ test/Voronoiarea.jl
@@ -7,7 +7,10 @@ x = 1.0 + rand(N)
 y = 1.0 + rand(N)
 
 A = VoronoiCells.voronoiarea(x, y, [LEFT; RIGHT; LOWER; UPPER])
+# For most realizations |sum(A) - 1| is approx 10^{-15} and easily
+# passing the test. Occasionally it fails with sum(A) = 0.999x, where x != 9
 @test_approx_eq sum(A) 1.0
+
 
 # ------------------------------------------------------------
 # Compare with Deldir
