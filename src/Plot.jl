a24e71982017e612a51e03820cacf1c5be054bb8
Robert Dahl Jacobsen <git@dahl-jacobsen.dk>
2021-01-21 21:46:07 +0100

    🔥 Remove unused method

--- src/Plot.jl
+++ src/Plot.jl
@@ -1,25 +1,3 @@
-function sides(tess::Tessellation)
-    n_points = mapreduce(length, +, tess.Cells)
-    sides = Vector{GeometryBasics.Line}(undef, n_points)
-
-    count = 0
-    for cell in tess.Cells
-        n_corners = length(cell)
-        for n in 2:n_corners
-            count += 1
-            # Ignore orientation of line
-            p1, p2 = minmax(cell[n-1], cell[n])
-            sides[count] = GeometryBasics.Line(p1, p2)
-        end
-
-        count += 1
-        sides[count] = GeometryBasics.Line(cell[n_corners], cell[1])
-    end
-
-    unique(sides)
-end
-
-
 function corner_coordinates(tess::Tessellation)
     # Each cell needs the number of points + line from last to first point + NaN
     n_points = mapreduce(x -> length(x) + 2, +, tess.Cells)
